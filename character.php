d4f8dd08cdca0064199586ac3549c401c250a912
maibelline quilliet <mayb80@hotmail.fr>
2022-04-05 12:44:33 +0200

    commit

---
+++ character.php
@@ -0,0 +1,37 @@
+<?php
+class Character{
+    //def des attributs de l'objet Character
+    protected int $_health;
+    protected int  $_rage;
+
+
+    // getter recuperer la valeur d'un attribut de l'exterieur de la classe // recupere l'attribut rage de la classe en cours
+    public function getHealth():int{
+        return $this->_health;
+    }
+    public function getRage():int{
+        return $this->_rage;
+    }
+
+    /** 
+     * setter  metho de visibilité public 
+     * @param int $health
+     * 
+     * @return void
+     */
+    public function setHealth (int $health): void{
+        $this->_healt= $health;
+    }
+    // def methode de visibilté public elle a pour paramétre d'entrée $age on affecte $age à l'attribut _age de l'objet en cours $this
+    public function setRage (int $rage): void{
+        $this->_rage= $rage;
+    }
+    //méthode magique construct visant à crér un personnage
+    public function __construct($health,$rage){
+        $this->_health= $health;
+        $this->_rage= $rage;
+    }
+}
+
+
+
